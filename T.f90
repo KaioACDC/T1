e42802e6e3abbef69211b9a32125fee58e028e6d
Kaio <kaio.chagas@ufv.br>
2024-11-23 01:23:52 -0300

    definindo dimensao dos valores

--- T.f90
+++ T.f90
@@ -2,4 +2,9 @@ program estatisticac
     use estatistica
     implicit none
 
+    real, allocatable, dimension(:) :: dados
+    real :: y, vvariancia, vdesvio
+    integer :: x
+    character(len=100) :: nomearquivo
+
     
